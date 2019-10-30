03bbe54f49f5e3347088c00c201a24839f4cc767
ravenusmc <mcuddy77@gmail.com>
2019-10-30 13:08:10 -0400

    worked on header

--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -1,10 +1,11 @@
 <template>
-
   <div>
     <header>
+      <div class='headerDiv'>
+        <h1>Iraq War Study</h1>
+      </div>
     </header>
   </div>
-
 </template>
 
 <script>
@@ -14,4 +15,25 @@ export default {
 </script>
 
 <style scoped>
+
+header {
+  background-image: url('../assets/img/flags.jpg');
+  background-position: center;
+  background-size: cover;
+  background-repeat: no-repeat;
+  height: 600px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+.headerDiv {
+  color: white;
+  background-color: rgba(0, 0, 0, 0.5);
+  padding: 10px;
+  border-radius: 10px;
+  text-transform: uppercase;
+}
+
 </style>
