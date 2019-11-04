797922a7190e3c1e68dcc1609db93b0a705767c3
ravenusmc <mcuddy77@gmail.com>
2019-11-04 13:53:34 -0500

    added new component

--- client/src/components/Main.vue
+++ client/src/components/Main.vue
@@ -2,17 +2,20 @@
   <div>
     <main>
       <SectionOne/>
+      <Study/>
     </main>
   </div>
 </template>
 
 <script>
 import SectionOne from '@/components/SectionOne.vue';
+import Study from '@/components/Study.vue';
 
 export default {
   name: 'Main',
   components: {
     SectionOne,
+    Study,
   },
 };
 </script>
---
+++ client/src/components/Study.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    Study Area
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Study',
+};
+</script>
+
+<style scoped>
+</style>
