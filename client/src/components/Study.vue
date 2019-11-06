6cb6b778c889482a611d509ec2bb127a41be3361
ravenusmc <mcuddy77@gmail.com>
2019-11-06 15:12:42 -0500

    working on form area

--- client/src/components/Study.vue
+++ client/src/components/Study.vue
@@ -1,12 +1,17 @@
 <template>
   <div>
-    Study Area
+    <Form/>
   </div>
 </template>
 
 <script>
+import Form from '@/components/subComponents/Form.vue';
+
 export default {
   name: 'Study',
+  components: {
+    Form,
+  },
 };
 </script>
 
---
+++ client/src/components/subComponents/Form.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>Form Area</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Form',
+};
+</script>
+
+<style scoped>
+</style>
