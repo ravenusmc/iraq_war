913f13dc3a9bd879118c4592588fcb2f34e8147f
ravenusmc <mcuddy77@gmail.com>
2019-11-07 16:11:55 -0500

    working on form

--- client/src/components/subComponents/Form.vue
+++ client/src/components/subComponents/Form.vue
@@ -1,12 +1,23 @@
 <template>
   <div>
-    <h1>Form Area</h1>
+    <form @submit="submitYears">
+      <h5>Select the Date by Year:</h5>
+      <label>Year One:</label>
+       <input type="number" v-model='yearOne' name="yearOne" placeholder="Enter First Year">&nbsp;
+       <label>Year Two:</label>
+       <input type="number" v-model="yearTwo" name="yearTwo" placeholder="Enter Second Year">
+       <v-btn type="submit" color='yellow red--text'>
+         Submit
+       </v-btn>
+    </form>
   </div>
 </template>
 
 <script>
 export default {
   name: 'Form',
+  methods: {
+  },
 };
 </script>
 
