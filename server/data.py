7452e0f85680a09e38507dc89a1e32486b21b743
ravenusmc <mcuddy77@gmail.com>
2019-11-13 14:19:32 -0500

    working more on getting dates set up

--- server/data.py
+++ server/data.py
@@ -16,12 +16,24 @@ class Data():
         first_time_stamp = pd.to_datetime(first_date)
         last_time_stamp = pd.to_datetime(last_date)
         self.data['Month'] = self.data['Date and time'].dt.month
+        self.data['Year'] = self.data['Date and time'].dt.year
         self.data = self.data.loc[(self.data['Date and time'] >= first_time_stamp) & (self.data['Date and time'] <= last_time_stamp), :]
-        #print(self.data)
+        print(self.data)
+        #Getting min year
+        min_year = self.data.Year.min()
+        #Getting Max year
+        max_year = self.data.Year.max()
         #I need to get the min value for the month column
         min_month = self.data.Month.min()
         #I need to get the max value for the month column
         max_month = self.data.Month.max()
+        #Use Coalition forces killed column
+        while min_year <= max_year:
+            while min_month <= max_month:
+
+                min_month += 1
+            min_year += 1
+
 
 # test = Data()
 # test.
