022d62462fe0a3f9b7dd38fede63f03c1d34fe5c
ravenusmc <mcuddy77@gmail.com>
2019-11-14 16:08:55 -0500

    still working on date issues

--- server/data.py
+++ server/data.py
@@ -15,24 +15,39 @@ class Data():
         #Converting what the user entered to proper datetime format
         first_time_stamp = pd.to_datetime(first_date)
         last_time_stamp = pd.to_datetime(last_date)
+
+        start_month = int(first_date[0])
+        start_year = int(first_date[4:8])
+        end_month = int(last_date[0])
+        end_year = int(last_date[4:8])
+
         self.data['Month'] = self.data['Date and time'].dt.month
         self.data['Year'] = self.data['Date and time'].dt.year
         self.data = self.data.loc[(self.data['Date and time'] >= first_time_stamp) & (self.data['Date and time'] <= last_time_stamp), :]
-        print(self.data)
-        #Getting min year
-        min_year = self.data.Year.min()
-        #Getting Max year
-        max_year = self.data.Year.max()
-        #I need to get the min value for the month column
-        min_month = self.data.Month.min()
-        #I need to get the max value for the month column
-        max_month = self.data.Month.max()
-        #Use Coalition forces killed column
-        while min_year <= max_year:
-            while min_month <= max_month:
 
-                min_month += 1
-            min_year += 1
+        #Getting min year
+        # min_year = self.data.Year.min()
+        # #Getting Max year
+        # max_year = self.data.Year.max()
+        # number_of_years = max_year - min_year
+        # #I need to get the min value for the month column
+        # min_month = self.data.Month.min()
+        # #I need to get the max value for the month column
+        # max_month = self.data.Month.max()
+
+        #while guess != name and pos < len(name):
+        #These loops will get the deaths by the year and then by the month
+        # while min_year <= max_year:
+        #     data = self.data
+        #     data =  data.loc[(data['Year'] == min_year)]
+        #     while min_month <= max_month:
+        #         month_data = data
+        #         month_data =  month_data.loc[(month_data['Month'] == min_month)]
+        #         monthly_deaths = month_data['Coalition forces killed'].sum()
+        #         min_month += 1
+        #     if number_of_years > 0:
+        #
+        #     min_year += 1
 
 
 # test = Data()
